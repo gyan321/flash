50290cb25fdcb899f1a51cf9168dcba81e6ba54f
gyan321 <46482936+gyan321@users.noreply.github.com>
2019-09-17 21:59:36 +0530

    first commit

---
+++ wsproject2/src/services/Score.java
@@ -0,0 +1,11 @@
+package services;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Score {
+	public int wins, losses, ties;
+}
